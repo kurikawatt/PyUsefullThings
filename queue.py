a0d2163b54d06bd5a95d03ffd802750895420234
François "Kurikawa" Odin <francois@kurikawa.fr>
2024-11-24 18:27:20 +0100

    Basic Queue class

---
+++ queue.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+
+from typing import Any
+
+class Queue:
+
+    def __init__(self):
+        self.__content = []
+        self.__size = 0
+
+    def __bool__(self) -> bool:
+        return self.__size != 0
+
+    def __len__(self) -> int:
+        return self.__size
+
+    def push(self, element:Any) -> None:
+        self.__content.append(element)
+        self.__size += 1
+
+    def pop(self) -> Any:
+        self.__size -= 1
+        return self.__content.pop(0)
