42058b2897c70fdce519ef40b1cb2c3f10503017
François "Kurikawa" Odin <francois@kurikawa.fr>
2024-11-24 15:21:16 +0100

    fixed memoize, now return the memoized func

--- memoize.py
+++ memoize.py
@@ -1,10 +1,13 @@
 #coding: utf-8
 
 def memoize(function):
-    memory = {}
     def memoized_function(*args):
         if args in memory:
             return memory[args]
         res = function(*args)
         memory[args] = res
         return res
+    
+    memory = {}
+    return memoized_function
+
