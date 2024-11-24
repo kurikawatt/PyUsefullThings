56539eb9d898d9169cd371ba42da4bc2e8efee42
François "Kurikawa" Odin <francois@kurikawa.fr>
2024-11-24 15:54:51 +0100

    docs for memoize

--- memoize.py
+++ memoize.py
@@ -1,13 +1,18 @@
 #coding: utf-8
 
 def memoize(function):
+    """
+    Ce décorateur permet d'utiliser la mémoization dans
+    une fonction récursive
+    """
+    # Le bout de code pour la rendre mémoizable
     def memoized_function(*args):
         if args in memory:
             return memory[args]
         res = function(*args)
         memory[args] = res
         return res
-    
+    # Création de la mémoire
     memory = {}
+    # On renvoit la fonction mémoizée
     return memoized_function
-
